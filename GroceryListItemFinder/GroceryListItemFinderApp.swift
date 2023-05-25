7eb545bd8590293d8fad3c683878ee8cddbcbf75
cbs9md <75951222+cbs9md@users.noreply.github.com>
2023-05-25 00:20:26 -0500

    Initial Commit

---
+++ GroceryListItemFinder/GroceryListItemFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListItemFinderApp.swift
+//  GroceryListItemFinder
+//
+//  Created by Chethan Shivaram on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct GroceryListItemFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
